210c232b03bd30428dbceafd18a9c08ff52a2da4
Rod McBride <rmcbride@toplinestrategies.com>
2017-12-13 14:39:46 -0700

    Added images and initial rendering of the quiz

--- js/app.js
+++ js/app.js
@@ -4,14 +4,26 @@
 
     var Quiz = React.createClass({
         propTypes: {
-            books: React.PropTypes.array.isRequired
+            data: React.PropTypes.array.isRequired
+        },
+        getInitialState: function () {
+            return this.props.data.selectGame();
         },
         render: function () {
-            return <div>
-                        {this.props.books.map(function(b) {
-                            return <Book title={b} />;
-                        })}
-                    </div>;
+            return (<div>
+                        <div className="row">
+                            <div className="col-md-4">
+                                <img src={this.state.author.imageUrl} className="authorimage col-md-3" />
+                            </div>
+                            <div className="col-md-7">
+                                {this.state.books.map(function(b) {
+                                    return <Book title={b} />;
+                                }, this)}
+                            </div>
+                            <div className="col-md-1"></div>
+                        </div>
+                    </div>
+            );
         }
     });
 
@@ -20,10 +32,69 @@
             title: React.PropTypes.string.isRequired
         },
         render: function () {
-            return <div><h4>{this.props.title}</h4></div>
+            return <div className="answer"><h4>{this.props.title}</h4></div>;
         }
     });
 
-    React.renderComponent(<Quiz books={['Lord of the Rings','The Iliad']} />, document.getElementById('app'));
+    // local data for testing
+    var data = [
+        {
+            name: 'Mark Twain', 
+            imageUrl: 'images/authors/marktwain.jpg',
+            books: ['The Adventures of Huckleberry Finn']
+        },
+        {
+            name: 'Joseph Conrad',
+            imageUrl: 'images/authors/josephconrad.png',
+            books: ['Heart of Darkness']
+        },
+        {
+            name: 'J.K. Rowling',
+            imageUrl: 'images/authors/jkrowling.jpg',
+            imageSource: 'Wikimedia Commons',
+            imageAttribution: 'Daniel Ogren',
+            books: ['Harry Potter and the Sorcerers Stone']
+        },
+        {
+            name: 'Stephen King',
+            imageUrl: 'images/authors/stephenking.jpg',
+            imageSource: 'Wikimedia Commons',
+            imageAttribution: 'Pinguino',
+            books: ['The Shining','IT']
+        },
+        {
+            name: 'Charles Dickens',
+            imageUrl: 'images/authors/charlesdickens.jpg',
+            imageSource: 'Wikimedia Commons',
+            books: ['David Copperfield', 'A Tale of Two Cities']
+        },
+        {
+            name: 'William Shakespeare',
+            imageUrl: 'images/authors/williamshakespeare.jpg',
+            imageSource: 'Wikimedia Commons',
+            books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+        }
+    ];
+
+    // add a method to initial the game with a random author, books and correct answer
+    data.selectGame = function () {
+        var books = _.shuffle(this.reduce(function (p, c, i) {
+            return p.concat(c.books);
+        }, [])).slice(0,4);
+
+        var answer = books[_.random(books.length-1)];
+
+        return {
+            books: books,
+            author: _.find(this, function (author) {
+                    return author.books.some(function (title) {
+                        return title === answer;
+                    });
+            })
+        };
+    };
+
+
+    React.renderComponent(<Quiz data={data} />, document.getElementById('app'));
 
 })();
