4a68f5f5cbcf8aa54704bc666fec5979603f36d3
Rod McBride <rmcbride@toplinestrategies.com>
2017-12-12 23:59:26 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/** @jsx React.DOM */
+(function () {
+    'use strict';
+
+    var Quiz = React.createClass({
+        propTypes: {
+            books: React.PropTypes.array.isRequired
+        },
+        render: function () {
+            return <div>
+                        {this.props.books.map(function(b) {
+                            return <Book title={b} />;
+                        })}
+                    </div>;
+        }
+    });
+
+    var Book = React.createClass({
+        propTypes: {
+            title: React.PropTypes.string.isRequired
+        },
+        render: function () {
+            return <div><h4>{this.props.title}</h4></div>
+        }
+    });
+
+    React.renderComponent(<Quiz books={['Lord of the Rings','The Iliad']} />, document.getElementById('app'));
+
+})();
